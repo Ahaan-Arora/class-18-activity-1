ac305212bd7fa184c8e00c01ff5324109983325b
Ahaan-Arora <90094350+Ahaan-Arora@users.noreply.github.com>
2021-09-30 17:16:30 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,27 @@
+
+class Student
+{
+   constructor(a,b,c){
+    
+    
+    this.name=a
+
+    this.age=b
+
+    this.grade=c
+
+   }
+
+   display(){
+
+    text("stutent name is: "+this.name,200,200)
+    text("stutent age is: "+this.age,200,220)
+    text("stutent grade is: "+this.grade,200,240)
+
+
+
+   }
+}
+
+
+
